4317e0d1afab145a63dcff089abb933085ab44ff
Kranthi Sedamaki <skranthi4444@gmail.com>
2023-03-04 22:40:13 +0530

    use geolocator instead of location

--- lib/widgets/get_location.dart
+++ lib/widgets/get_location.dart
@@ -2,9 +2,9 @@ import 'package:campus_transit/utils/point.dart';
 import 'package:campus_transit/utils/sheets_manager.dart';
 import 'package:campus_transit/widgets/destination_block.dart';
 import 'package:campus_transit/widgets/to_destination.dart';
-import 'package:location/location.dart';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 final coordinates = {
   'Main Gate':
@@ -22,137 +22,109 @@ class GetLocation extends StatefulWidget {
 }
 
 class _GetLocationState extends State<GetLocation> {
-  bool _serviceEnabled = false;
-  PermissionStatus _permissionGranted = PermissionStatus.denied;
-
-  final _location = Location();
   Points _closestPoint = Points.mainGate;
 
   TransportScheduleRow? _destinationOne;
   TransportScheduleRow? _destinationTwo;
 
   final _sheetManager = TransportScheduleSheetManager();
-  late Future<void> _init;
-  // bool _isLoaded = false;
+
+  late Future<Position> _initLocation;
 
   @override
   void initState() {
-    _init = initializeLocation();
+    _initLocation = _initializeLocation();
     super.initState();
   }
 
-  Future<void> initializeLocation() async {
-    final isServiceEnabled = await _location.serviceEnabled();
+  Future<Position> _initializeLocation() async {
+    bool serviceEnabled = false;
+    LocationPermission permission = LocationPermission.denied;
 
-    final hasPermission = await _location.hasPermission();
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
 
-    final rows = await _getClosestPoint();
-
-    _serviceEnabled = isServiceEnabled;
-    _permissionGranted = hasPermission;
-    _destinationOne = rows[0];
-    _destinationTwo = rows[1];
-  }
-
-  Future<void> _enableLocation() async {
-    await _location.requestService().then((value) => setState(() {
-          _serviceEnabled = value;
-        }));
-  }
-
-  Future<PermissionStatus> _getPermission() async {
-    PermissionStatus status = await _location.hasPermission();
-    if (status == PermissionStatus.denied) {
-      status = await _location.requestPermission();
-      if (status != PermissionStatus.granted) {
-        return status;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
       }
     }
-    return status;
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    final currLocation = await Geolocator.getCurrentPosition();
+
+    final rows = await _getClosestPoint(currLocation);
+    _destinationOne = rows[0];
+    _destinationTwo = rows[1];
+
+    return currLocation;
   }
 
-  Future<List<TransportScheduleRow>> _getClosestPoint() async {
-    final location = await _location.getLocation();
-    final currentCoordinate = Coordinate(
-        latitude: location.latitude!, longitude: location.longitude!);
-    final distances = coordinates.map((key, value) {
-      return MapEntry(key, value.distanceTo(currentCoordinate));
-    });
-    final minDistPoint = distances.entries.reduce((a, b) {
-      return a.value < b.value ? a : b;
-    });
-    final source = stringToPoint[minDistPoint.key]!;
-    setState(() {
-      _closestPoint = source;
-    });
-    final otherTwoPoints =
-        Points.values.where((element) => element != source).toList();
+  Future<List<TransportScheduleRow>> _getClosestPoint(Position position) async {
+    final currentLocation =
+        Coordinate(latitude: position.latitude, longitude: position.longitude);
 
-    List<TransportScheduleRow> rows = [];
+    final distances = coordinates.map((key, value) {
+      return MapEntry(key, value.distanceTo(currentLocation));
+    });
+
+    final closestPoint = distances.entries.reduce((value, element) {
+      return value.value < element.value ? value : element;
+    });
+
+    _closestPoint = Points.values.firstWhere(
+        (element) => element.string == closestPoint.key,
+        orElse: () => Points.mainGate);
+
+    final otherTwoPoints =
+        Points.values.where((element) => element != _closestPoint).toList();
+    final List<TransportScheduleRow> rows = [];
+
     for (final point in otherTwoPoints) {
-      TransportScheduleRow? destination;
-      destination =
-          await _sheetManager.nextVehicleToDrop(source, point, DateTime.now());
-      rows.add(destination);
+      final row = await _sheetManager.nextVehicleToDrop(
+          _closestPoint, point, DateTime.now());
+      rows.add(row);
     }
     return rows;
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!_serviceEnabled) {
-      return Center(
-        child: Column(
-          children: [
-            const Text("Please enable location services to use"),
-            ElevatedButton(
-              onPressed: _enableLocation,
-              child: const Text("Enable Location"),
-            )
-          ],
-        ),
-      );
-    }
-
-    if (_permissionGranted == PermissionStatus.denied) {
-      return Center(
-        child: Column(
-          children: [
-            const Text("Please grant location permsission to use"),
-            ElevatedButton(
-              onPressed: () async {
-                final status = await _getPermission();
-                setState(() {
-                  _permissionGranted = status;
-                });
-              },
-              child: const Text("Request Permission"),
-            )
-          ],
-        ),
-      );
-    }
-
-    if (_permissionGranted == PermissionStatus.deniedForever) {
-      return const Center(
-        child: Text("Please grant location permsission to use"),
-      );
-    }
-
     return FutureBuilder(
-      future: _init,
+      future: _initLocation,
       builder: (ctx, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(
             child: CircularProgressIndicator(),
           );
         }
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("${snapshot.error}"),
+          );
+        }
+
+        if (snapshot.data == null) {
+          return const Center(
+            child: Text("No location data"),
+          );
+        }
+
         return RefreshIndicator(
             color: const Color(0xFF514C5E),
-            onRefresh: () => _getClosestPoint().then((value) => setState(() {
-                  _destinationOne = value[0];
-                  _destinationTwo = value[1];
-                })),
+            onRefresh: () =>
+                _getClosestPoint(snapshot.data!).then((value) => setState(() {
+                      _destinationOne = value[0];
+                      _destinationTwo = value[1];
+                    })),
             child: Center(
                 child: ListView(children: [
               DestinationBlock(
